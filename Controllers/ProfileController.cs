653c1a41675f4e230c982955a786ca0b0d530a26
Admin <vanyasoroka2005@mail.ru>
2025-05-15 19:00:29 +0300

    Profile page and avatar features

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -22,4 +22,31 @@ public class ProfileController : Controller
 
         return View(user);
     }
+    [HttpPost]
+    public async Task<IActionResult> UploadAvatar(IFormFile avatar)
+    {
+        int? userId = HttpContext.Session.GetInt32("UserId");
+        if (userId == null || avatar == null)
+            return RedirectToAction("Index");
+
+        var user = _context.Users.FirstOrDefault(u => u.id == userId);
+        if (user == null)
+            return NotFound();
+
+        var fileName = $"{Guid.NewGuid()}{Path.GetExtension(avatar.FileName)}";
+        var savePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "avatars", fileName);
+
+        using (var stream = new FileStream(savePath, FileMode.Create))
+        {
+            await avatar.CopyToAsync(stream);
+        }
+
+        user.image = "/avatars/" + fileName;
+        _context.SaveChanges();
+
+        HttpContext.Session.SetString("UserImage", user.image); 
+
+        return RedirectToAction("Index");
+    }
+
 }
